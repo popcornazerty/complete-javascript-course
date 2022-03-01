a8eefa67d33cf313b35f74f546bb254f8f10f6dd
popcornazerty <78792807+popcornazerty@users.noreply.github.com>
2022-03-01 12:13:55 +0100

    update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-let hasDriversLicense = false;
+/*let hasDriversLicense = false;
 const passTest = true;
 
 if(passTest) hasDriversLicense = true;
@@ -25,16 +25,82 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
 
-
-const fuel = false;
-function repareCar(tires, $fuel) {
-    let goRepare;
-    if(fuel === false) {
-    console.log(`You need to the repre shop and change your ${tires} tires.`);
-    } else {
-    console.log(`You need to the repre shop and change your ${tires} tires and ${fuel} fuel.`);
-    return goRepare;
+// function declaration
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
 }
- 
-repareCar(2,$fuel);
-console.log(repareCar);
+const age1 = calcAge1(1989);
+console.log(age1);
+
+// function expression
+const calcAge2 = function(birthYear) {
+    return 2022 - birthYear;
+}
+const age2 = calcAge2(2000);
+
+console.log(age1,age2);
+
+// Arrow function
+const calcAge3 = birthYear => 2022 - birthYear;
+const Age3 = calcAge3(1989);
+console.log(Age3);
+
+// retirementDate => retirementDate - 2015;
+// how many years a person has left until retirement
+const calcYears = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    const retirementDate = 65 - age;
+    // return retirementDate;
+    return `${firstName} retires in ${retirementDate} years`;
+}
+console.log(calcYears(1989,'Nicolas'));
+*/
+
+function cutMachine(fruit) {
+  return fruit * 4;
+}
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutMachine(apples);
+  const orangePieces = cutMachine(oranges);
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+// Arrow function
+const calcAge3 = (birthYear) => 2022 - birthYear;
+const Age3 = calcAge3(1989);
+
+// retirementDate => retirementDate - 2015;
+// how many years a person has left until retirement
+const calcYears = (birthYear, firstName) => {
+  // const age = 2022 - birthYear;
+  const age = calcAge3(birthYear);
+  const retirementDate = 65 - age;
+  // return retirementDate;
+  if (retirementDate > 0) {
+    console.log(`${firstName} retires in ${retirementDate} years.` );
+    return retirementDate;
+   } else {
+    console.log(`${firstName} already retired ${retirementDate} years ago, lucky him :P.`);
+    return -1;
+  }
+};
+console.log(calcYears(1989, "Nicolas"));
+console.log(calcYears(1950, "Paul"));
+
+
+
+function repareCar(isTiresGood, isFuel) {
+
+    if(isFuel === false || isTiresGood === false){
+
+        return alert("You need to go the repair shop");
+    }
+
+    return "Your car is Good right now;"
+
+}
+
+repareCar(false, true);
+
